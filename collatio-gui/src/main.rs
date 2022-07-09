cce3e8bb4b424702ed8b9050fcc22c5556cc58a0
Ben <ben@thedesigncollaborative.co>
2022-07-09 19:14:32 +0100

    feat: initial commit

---
+++ collatio-gui/src/main.rs
@@ -0,0 +1,19 @@
+mod widget;
+
+use druid::widget::{Label, Button, Flex};
+use druid::{AppLauncher, PlatformError, WindowDesc, Widget};
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(ui_builder());
+    let data = 0_u32;
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(data)
+}
+
+// This is where the application starts
+fn ui_builder() -> impl Widget<u32> {
+    let button = Button::new("hello");
+
+    Flex::column().with_child(button)
+}
